cc6d2b15e94048bf954ec2b9ea1da57ecf6f3404
Syed Muneeb <syedmuneebahmed40@gmail.com>
2026-08-04 14:02:31 +0500

    make changes in SecurityConfig

--- Backend/flash_ticket_system/src/main/java/com/zynvora/flash_ticket_system/Configuration/SecurityConfig.java
+++ Backend/flash_ticket_system/src/main/java/com/zynvora/flash_ticket_system/Configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.zynvora.flash_ticket_system.Configuration;
 
 import com.zynvora.flash_ticket_system.Security.CustomUserDetailService;
+import com.zynvora.flash_ticket_system.Security.JwtAuthenticationFilter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CustomUserDetailService customUserDetailService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
@@ -27,6 +30,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable() )
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(daoAuthenticationProvider())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         .authorizeHttpRequests(req -> req
             .requestMatchers("/auth/**").permitAll()
             .requestMatchers("/admin/**").hasRole("ADMIN")
